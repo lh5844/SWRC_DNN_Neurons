ec38f896cda758740caee721ab3c079f7353753b
Lucy Hu <lh5844@rit.edu>
2024-12-03 15:08:14 -0500

    removed extra DNN model to focus on 5 hidden layer

--- DNN_Wetting_WaterContYAxis.py
+++ DNN_Wetting_WaterContYAxis.py
@@ -20,10 +20,8 @@ set_random_seed(2)
  
 # 2. Define learning parameters i.e. learning rate (for gradient descent) & the number of steps (epochs)
 learning_rate   = 0.01
-learning_rate_ANN   = 0.01
 momentum        = 0.1 #Optional --- used if using tensorflow "MomentumOptimizer" instead of "GradientDescentOptimizer"
 training_epochs =5000
-training_epochs_Art =5000
  
 # 3. Import the soil Data (measured/observed suction and soil moisture)
 soilData = pd.read_csv('Test_data/Wetting2.csv')
@@ -64,43 +62,6 @@ Y = tf.placeholder("float")
 
 # 8. setup the models: DNN models
  
-# Deep NN model generation helper function
-def Deep_NN(x, weights, biases):
-     
-    # hidden layer #1 with RELU: layer_1=relu(W1*x+b1)
-	# hidden layer #2 with RELU: layer_2=relu(W2*layer_1+b2)
-	# hidden layer #3 with RELU: layer_3=relu(W3*layer_2+b3)
-	# hidden layer #4 with RELU: layer_4=relu(W4*layer_3+b4)
-	# hidden layer #5 with RELU: layer_5=relu(W5*layer_4+b5)
-	# output layer #6 with linear: layer_6=linear(W_out*layer_5+b_out)	
-
-    reshaped_x = tf.reshape(x, [-1, 1])
-    layer_1 = tf.add(tf.matmul(reshaped_x, weights['h1']), biases['b1'])
-    layer_1 = tf.nn.relu(layer_1)
-      
-    # hidden layer #2 with RELU: layer_2=relu(W2*layer1+b2)
-    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-    layer_2 = tf.nn.relu(layer_2)
-	
-	# hidden layer #3 with RELU: layer_2=relu(W2*layer1+b2)
-    layer_3 = tf.add(tf.matmul(layer_2, weights['h3']), biases['b3'])
-    layer_3 = tf.nn.relu(layer_3)
-	
-	# hidden layer #4 with RELU: layer_2=relu(W2*layer1+b2)
-    layer_4 = tf.add(tf.matmul(layer_3, weights['h4']), biases['b4'])
-    layer_4 = tf.nn.relu(layer_4)
-
-	# hidden layer #4 with RELU: layer_2=relu(W2*layer1+b2)
-    layer_5 = tf.add(tf.matmul(layer_4, weights['h5']), biases['b5'])
-    layer_5 = tf.nn.relu(layer_5)
-	
-    # output layer with linear activation (no RELUs!): out_layer=W_out*layer2+b_out
-    out_layer = tf.matmul(layer_5, weights['out']) + biases['out']
-      
-    # return the DNN model
-    return out_layer
-
-
 def Deep_NN_5HiddenLayers(x, weights5, biases5):
      
     # hidden layer #1 with RELU: layer_1=relu(W1*x+b1)
@@ -181,46 +142,26 @@ biases5 = {
 }
 
 # 11. Now use helper function that was previously defined to generate a DNN models
-	#ANN and DNN
-pred = Deep_NN(X, weights, biases) 
 
 	#5 Hidden Layers
 pred_5Layers_DNN = Deep_NN_5HiddenLayers(X, weights5, biases5)
 
-# 12. Define the DNN/ANN cost function that needs to be optimzed in order to get optimal weights and biases for the DNN/ANN layers:
+# 12. Define the DNN cost function that needs to be optimzed in order to get optimal weights and biases for the DNN layers:
 # This is acieved by minimizing the sum of squared errors (SSE)
 n_samples=len(test_X)
-cost = tf.reduce_sum(tf.pow(pred-Y, 2))/(2*n_samples)
 
 cost5 = tf.reduce_sum(tf.pow(pred_5Layers_DNN -Y, 2))/(2*n_samples) 
 
 # 13. Generate optimizer node for gradient descent algorithm using tensorflow GradientDescentOptimizer() function
-#optimizer = tf.train.MomentumOptimizer(learning_rate, momentum).minimize(cost)  #Method 1
-optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)		 #Method 2
 optimizer5 = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost5)
  
 # 14. initialize the variables (to be optimized) & launch optimization 
-init = tf.initialize_all_variables()
-sess = tf.InteractiveSession()
-sess.run(init)
-
 init5 = tf.initialize_all_variables()
 sess5 = tf.InteractiveSession()
 sess5.run(init5) 
 
 
-# 15. Kick off the training of the ANN/DNN models ...
-print("Starting with training in Deep_NN...")
-for epoch in range(training_epochs):
-     # for all individual data samples we have ...  
-    for (x, y) in zip(train_X, train_Y):
-        # run a backprob step
-        sess.run(optimizer, feed_dict={X: x, Y: y})        
-    # display epoch nr
-    if epoch % 20 == 0:
-        c = sess.run(cost, feed_dict={X: test_X, Y:test_Y})
-        print("Epoch:", '%04d' % epoch, "cost=", "{:.9f}".format(c))
-print("Deep_NN Optimization Finished!")
+# 15. Kick off the training of the DNN models ...
 
 print("Starting with training in Deep_NN_5HiddenLayers...")
 for epoch in range(training_epochs):
@@ -231,27 +172,16 @@ for epoch in range(training_epochs):
     # display epoch nr
     if epoch % 20 == 0:
         c5 = sess5.run(cost5, feed_dict={X: test_X, Y:test_Y})
-        print("Epoch:", '%04d' % epoch, "cost=", "{:.9f}".format(c))
+        print("Epoch:", '%04d' % epoch, "cost=", "{:.9f}".format(c5))
 print("Deep_NN_5HiddenLayers Optimization Finished!")
 
  
 # 16. Display final costs
-test_cost = sess.run(cost, feed_dict={X: test_X, Y: test_Y})
-print("Test dataset DNN costs=", test_cost, "\n")
 
 test_cost5 = sess5.run(cost5, feed_dict={X: test_X, Y: test_Y})
 print("Test dataset DNN5 costs=", test_cost5, "\n")
  
 # 17. Calculate the prediction by DNN models 
-predicted_Y = numpy.zeros(n_samples)
-for i in range(len(test_X)):
-    y_value = sess.run(pred, feed_dict={X: test_X[i]})
-    #print("i=",i, "x=",test_X[i], "f(x)=",y_value)
-    predicted_Y[i] = y_value
-
-predicted_Y = predicted_Y.reshape(-1, 1)
-predicted_Y=sc_y.inverse_transform(predicted_Y)
-#print(predicted_Y)
 
 predicted_DNN5_Y = numpy.zeros(n_samples)
 for i in range(len(test_X)):
@@ -267,18 +197,16 @@ print(pd.DataFrame(predicted_DNN5_Y).dtypes)
 # 18. Save predicted SWC
 
 predicted_SWC=soilData.copy()
-predicted_SWC['SWC_DNN']=pd.DataFrame(predicted_Y).iloc[:, 0:1].values
 predicted_SWC['SWC_DNN_5HiddenLayers']=pd.DataFrame(predicted_DNN5_Y).iloc[:, 0:1].values
 
 print(predicted_SWC)
 predicted_SWC.to_csv('./Output_files/DNN_Layer5_predicted_Wetting2_SWC_Epoch=5000.csv', encoding='utf-8', index=False)
 
 # Willmot's index of agreement d1
-d1_DNN = 1-(((soilData['Observed Water Content']-predicted_SWC['SWC_DNN']).sum())/(abs(soilData['Observed Water Content']-y_mean)+abs(predicted_SWC['SWC_DNN']-y_mean)).sum())
 d1_DNN5 = 1-(((soilData['Observed Water Content']-predicted_SWC['SWC_DNN_5HiddenLayers']).sum())/(abs(soilData['Observed Water Content']-y_mean)+abs(predicted_SWC['SWC_DNN_5HiddenLayers']-y_mean)).sum())
 
 
-print(': and d1_DNN='+str(d1_DNN)+ ': and d1_DNN5='+str(d1_DNN5))
+print('d1_DNN5='+str(d1_DNN5))
 
 # 19. plot measured/observed SWC/SWRC and ANN/DNN predicted SWC/SWRC
 
