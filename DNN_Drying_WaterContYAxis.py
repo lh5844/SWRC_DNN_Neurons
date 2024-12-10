79288c7c7427e47a6a30fa0cbe09c227918e0254
Harbor <hmw2331@rit.edu>
2024-12-09 22:12:52 -0500

    Ran experiment, created graphical output

--- DNN_Drying_WaterContYAxis.py
+++ DNN_Drying_WaterContYAxis.py
@@ -1,4 +1,5 @@
 # 1.1 Import the primary libraries 
+import json
 import math
 import tensorflow.compat.v1 as tf
 import numpy
@@ -23,7 +24,7 @@ set_random_seed(2)
 # 2. Define learning parameters i.e. learning rate (for gradient descent) & the number of steps (epochs)
 learning_rate   = 0.01
 momentum        = 0.1 #Optional --- used if using tensorflow "MomentumOptimizer" instead of "GradientDescentOptimizer"
-training_epochs =500 ## TODO UPDATE THIS!! Small number for testing purposes
+training_epochs =5000 ## TODO UPDATE THIS!! Small number for testing purposes
 
 # 3. Import the soil Data (measured/observed suction and soil moisture)
 
@@ -100,166 +101,177 @@ def Deep_NN_5HiddenLayers(x, weights5, biases5):
     return out_layer
 
 
-costs = {3: [], 5: [], 7: [], 9: [], 11: []}
-timeToRun = {3: [None, None], 5: [None, None], 7: [None, None], 9: [None, None], 11: [None, None]}
+# costs = {3: [], 5: [], 7: [], 9: [], 11: []}
+# timeToRun = {3: [None, None], 5: [None, None], 7: [None, None], 9: [None, None], 11: [None, None]}
 # 9. Specify the number of neurons per layer i.e. neurons in input layer, hidden layer(s), and output layer
-for num_neurons in [3,5,7,9,11]:
-    start = datetime.now()
-    dim_in = 1
-    dim1 = num_neurons
-    dim2 = num_neurons
-    dim3 = num_neurons
-    dim4 = num_neurons
-    dim5 = num_neurons
-    dim_out = 1
-        
-    # 10. Create dictionaries to hold the weights & biases of all layers
-    weights = {
-        'h1': tf.Variable(tf.random_normal([dim_in, dim1])),
-        'h2': tf.Variable(tf.random_normal([dim1, dim2])),
-        'h3': tf.Variable(tf.random_normal([dim2, dim3])),
-        'h4': tf.Variable(tf.random_normal([dim3, dim4])),
-        'h5': tf.Variable(tf.random_normal([dim4, dim5])),
-        'out': tf.Variable(tf.random_normal([dim2, dim_out]))
-    }
-    biases = {
-        'b1': tf.Variable(tf.random_normal([dim1])),
-        'b2': tf.Variable(tf.random_normal([dim2])),
-        'b3': tf.Variable(tf.random_normal([dim3])),
-        'b4': tf.Variable(tf.random_normal([dim4])),
-        'b5': tf.Variable(tf.random_normal([dim5])),
-        'out': tf.Variable(tf.random_normal([dim_out]))
-    }
-
-    weights5 = {
-        'h1': tf.Variable(tf.random_normal([dim_in, dim1])),
-        'h2': tf.Variable(tf.random_normal([dim1, dim2])),
-        'h3': tf.Variable(tf.random_normal([dim2, dim3])),
-        'h4': tf.Variable(tf.random_normal([dim3, dim4])),
-        'h5': tf.Variable(tf.random_normal([dim4, dim5])),
-        'out': tf.Variable(tf.random_normal([dim2, dim_out]))
-    }
-    biases5 = {
-        'b1': tf.Variable(tf.random_normal([dim1])),
-        'b2': tf.Variable(tf.random_normal([dim2])),
-        'b3': tf.Variable(tf.random_normal([dim3])),
-        'b4': tf.Variable(tf.random_normal([dim4])),
-        'b5': tf.Variable(tf.random_normal([dim5])),
-        'out': tf.Variable(tf.random_normal([dim_out]))
-    }
-
-    # 11. Now use helper function that was previously defined to generate a DNN models
-
-        #5 Hidden Layers
-    pred_5Layers_DNN = Deep_NN_5HiddenLayers(X, weights5, biases5)
-
-    # 12. Define the DNN cost function that needs to be optimzed in order to get optimal weights and biases for the DNN layers:
-    # This is acieved by minimizing the sum of squared errors (SSE)
-    n_samples=len(test_X)
-
-    cost5 = tf.reduce_sum(tf.pow(pred_5Layers_DNN -Y, 2))/(2*n_samples) 
-
-    # 13. Generate optimizer node for gradient descent algorithm using tensorflow GradientDescentOptimizer() function
-
-    optimizer5 = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost5)
+num_neurons = 11
+start = datetime.now()
+dim_in = 1
+dim1 = num_neurons
+dim2 = num_neurons
+dim3 = num_neurons
+dim4 = num_neurons
+dim5 = num_neurons
+dim_out = 1
     
-    # 14. initialize the variables (to be optimized) & launch optimization 
+# 10. Create dictionaries to hold the weights & biases of all layers
+weights = {
+    'h1': tf.Variable(tf.random_normal([dim_in, dim1])),
+    'h2': tf.Variable(tf.random_normal([dim1, dim2])),
+    'h3': tf.Variable(tf.random_normal([dim2, dim3])),
+    'h4': tf.Variable(tf.random_normal([dim3, dim4])),
+    'h5': tf.Variable(tf.random_normal([dim4, dim5])),
+    'out': tf.Variable(tf.random_normal([dim2, dim_out]))
+}
+biases = {
+    'b1': tf.Variable(tf.random_normal([dim1])),
+    'b2': tf.Variable(tf.random_normal([dim2])),
+    'b3': tf.Variable(tf.random_normal([dim3])),
+    'b4': tf.Variable(tf.random_normal([dim4])),
+    'b5': tf.Variable(tf.random_normal([dim5])),
+    'out': tf.Variable(tf.random_normal([dim_out]))
+}
 
-    init5 = tf.initialize_all_variables()
-    sess5 = tf.InteractiveSession()
-    sess5.run(init5) 
+weights5 = {
+    'h1': tf.Variable(tf.random_normal([dim_in, dim1])),
+    'h2': tf.Variable(tf.random_normal([dim1, dim2])),
+    'h3': tf.Variable(tf.random_normal([dim2, dim3])),
+    'h4': tf.Variable(tf.random_normal([dim3, dim4])),
+    'h5': tf.Variable(tf.random_normal([dim4, dim5])),
+    'out': tf.Variable(tf.random_normal([dim2, dim_out]))
+}
+biases5 = {
+    'b1': tf.Variable(tf.random_normal([dim1])),
+    'b2': tf.Variable(tf.random_normal([dim2])),
+    'b3': tf.Variable(tf.random_normal([dim3])),
+    'b4': tf.Variable(tf.random_normal([dim4])),
+    'b5': tf.Variable(tf.random_normal([dim5])),
+    'out': tf.Variable(tf.random_normal([dim_out]))
+}
 
+# 11. Now use helper function that was previously defined to generate a DNN models
 
-    # 15. Kick off the training of the DNN models ...
+    #5 Hidden Layers
+pred_5Layers_DNN = Deep_NN_5HiddenLayers(X, weights5, biases5)
 
-    print(f"Starting with training in {num_neurons}neurons_Deep_NN_5HiddenLayers...")
-    for epoch in range(training_epochs):
-        # for all individual data samples we have ...  
-        for (x, y) in zip(train_X, train_Y):
-            # run a backprob step
-            sess5.run(optimizer5, feed_dict={X: x, Y: y})       
-        # display epoch nr
-        if epoch % 20 == 0:
-            c5 = sess5.run(cost5, feed_dict={X: test_X, Y:test_Y})
-            print("Epoch:", '%04d' % epoch, "cost=", "{:.9f}".format(c5))
-            costs[num_neurons].append(c5)
-    print("Deep_NN_5HiddenLayers Optimization Finished!")
-    end = datetime.now()
-    timeToRun[num_neurons][0] = (end-start).seconds
+# 12. Define the DNN cost function that needs to be optimzed in order to get optimal weights and biases for the DNN layers:
+# This is acieved by minimizing the sum of squared errors (SSE)
+n_samples=len(test_X)
 
-    # 16. Display final costs
+cost5 = tf.reduce_sum(tf.pow(pred_5Layers_DNN -Y, 2))/(2*n_samples) 
 
-    test_cost5 = sess5.run(cost5, feed_dict={X: test_X, Y: test_Y})
-    print("Test dataset DNN5 costs=", test_cost5, "\n")
+# 13. Generate optimizer node for gradient descent algorithm using tensorflow GradientDescentOptimizer() function
 
-    # 17. Calculate the prediction by ANN/DNN models 
-    start = datetime.now()
-    predicted_DNN5_Y = numpy.zeros(n_samples)
-    for i in range(len(test_X)):
-        y_value5 = sess5.run(pred_5Layers_DNN, feed_dict={X: test_X[i]})
-        #print("i=",i, "x=",test_X[i], "f(x)=",y_value)
-        predicted_DNN5_Y[i] = y_value5
+optimizer5 = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost5)
 
-    predicted_DNN5_Y = predicted_DNN5_Y.reshape(-1, 1)
-    predicted_DNN5_Y=sc_y.inverse_transform(predicted_DNN5_Y)
-    print(pd.DataFrame(predicted_DNN5_Y)) 
-    print(pd.DataFrame(predicted_DNN5_Y).dtypes)
-    end = datetime.now()
-    timeToRun[num_neurons][1] = (end-start).seconds
+# 14. initialize the variables (to be optimized) & launch optimization 
 
-    # 18. Save predicted SWC
-
-    predicted_SWC=soilData.copy()
-    predicted_SWC['SWC_DNN_5HiddenLayers']=pd.DataFrame(predicted_DNN5_Y).iloc[:, 0:1].values
-
-    print(predicted_SWC)
-    predicted_SWC.to_csv('./Output_files/DNN_Layer5_predicted_Drying1_SWC_Epoch=5000.csv', encoding='utf-8', index=False)
-
-    # Willmot's index of agreement d1
-    d1_DNN5 = 1-(((soilData['Observed Water Content']-predicted_SWC['SWC_DNN_5HiddenLayers']).sum())/(abs(soilData['Observed Water Content']-y_mean)+abs(predicted_SWC['SWC_DNN_5HiddenLayers']-y_mean)).sum())
-
-    print('d1_DNN5='+str(d1_DNN5))
-
-
-    # 19. plot measured/observed SWC/SWRC and DNN predicted SWC/SWRC
-
-    fig = plt.figure()
-    ax = plt.subplot(111)
-
-    plt.scatter(soilData.iloc[:, 0:1].values, soilData.iloc[:, 2:3].values, label='Observed Data, '+'N='+str(n_samples), facecolors='none', edgecolors='r')
-
-    plt.semilogy(predicted_DNN5_Y, soilData.iloc[:, 2:3].values, 'k^' ,label=f'{num_neurons}neurons_DNN5: $R^2=$' + str(round(np.sqrt(r2_score(soilData.iloc[:, 0:1].values, predicted_DNN5_Y)),4))+ ', RMSE='+str(round(np.sqrt(mean_squared_error(soilData.iloc[:, 0:1].values, predicted_DNN5_Y)),3))  + '$cm^3$/$cm^3$'+', d1='+str(round(d1_DNN5,4)))
-
-    plt.xlabel('Soil Water Content ($cm^3$/$cm^3$)')
-    plt.ylabel('Suction (cm)')
-    #plt.xlim([0, 0.5])
-    # plt.ylim([0, 100])
-    from matplotlib.ticker import LogLocator
-
-    #plt.legend(loc='lower left')
-    #plt.legend(loc='upper right')
-
-    ax.legend(ncol=2,handleheight=2.1, labelspacing=0.05, bbox_to_anchor=(0.5, -0.1), loc='upper center')
+init5 = tf.initialize_all_variables()
+sess5 = tf.InteractiveSession()
+sess5.run(init5) 
 
 
-    plt.tight_layout()
-    # 20. Saave the plots (sic)
-    plt.savefig(f'./Output_files/{num_neurons}neurons_NeuralNetwork_Drying1_SWC_5Hidden-Layers_Epoch={training_epochs}.png', bbox_inches="tight")
-    ## TODO Do we really need PDFs also?
-    # plt.savefig(f'./Output_files/{num_neurons}neurons_NeuralNetwork_Drying1_SWC_5Hidden-Layers_Epoch={training_epochs}.pdf', bbox_inches="tight")
-    # plt.show()
-    sess5.close()
+# 15. Kick off the training of the DNN models ...
+costsSeries = []
+print(f"Starting with training in {num_neurons}neurons_Deep_NN_5HiddenLayers...")
+for epoch in range(training_epochs):
+    # for all individual data samples we have ...  
+    for (x, y) in zip(train_X, train_Y):
+        # run a backprob step
+        sess5.run(optimizer5, feed_dict={X: x, Y: y})       
+    # display epoch nr
+    if epoch % 20 == 0:
+        c5 = sess5.run(cost5, feed_dict={X: test_X, Y:test_Y})
+        print("Epoch:", '%04d' % epoch, "cost=", "{:.9f}".format(c5))
+        costsSeries.append(c5)
+print("Deep_NN_5HiddenLayers Optimization Finished!")
+end = datetime.now()
+trainTime = (end-start).seconds
+
+# 16. Display final costs
+
+test_cost5 = sess5.run(cost5, feed_dict={X: test_X, Y: test_Y})
+print("Test dataset DNN5 costs=", test_cost5, "\n")
+
+# 17. Calculate the prediction by ANN/DNN models 
+start = datetime.now()
+predicted_DNN5_Y = numpy.zeros(n_samples)
+for i in range(len(test_X)):
+    y_value5 = sess5.run(pred_5Layers_DNN, feed_dict={X: test_X[i]})
+    #print("i=",i, "x=",test_X[i], "f(x)=",y_value)
+    predicted_DNN5_Y[i] = y_value5
+
+predicted_DNN5_Y = predicted_DNN5_Y.reshape(-1, 1)
+predicted_DNN5_Y=sc_y.inverse_transform(predicted_DNN5_Y)
+print(pd.DataFrame(predicted_DNN5_Y)) 
+print(pd.DataFrame(predicted_DNN5_Y).dtypes)
+end = datetime.now()
+testTime = (end-start).seconds
+
+# 18. Save predicted SWC
+
+predicted_SWC=soilData.copy()
+predicted_SWC['SWC_DNN_5HiddenLayers']=pd.DataFrame(predicted_DNN5_Y).iloc[:, 0:1].values
+
+print(predicted_SWC)
+predicted_SWC.to_csv('./Output_files/DNN_Layer5_predicted_Drying1_SWC_Epoch=5000.csv', encoding='utf-8', index=False)
+
+# Willmot's index of agreement d1
+d1_DNN5 = 1-(((soilData['Observed Water Content']-predicted_SWC['SWC_DNN_5HiddenLayers']).sum())/(abs(soilData['Observed Water Content']-y_mean)+abs(predicted_SWC['SWC_DNN_5HiddenLayers']-y_mean)).sum())
+
+print('d1_DNN5='+str(d1_DNN5))
+
+
+# 19. plot measured/observed SWC/SWRC and DNN predicted SWC/SWRC
 
 fig = plt.figure()
 ax = plt.subplot(111)
-for (k,v) in costs.items():
-    plt.plot([i for i in range(0,training_epochs,20)], [i for i in map(lambda x: math.log(x, 10), v)], label=f"{k} Neurons")
-plt.title(f"Convergence Times")
-plt.xlabel("Epochs")
-plt.legend()
-plt.ylabel("log_10(Cost)")
-plt.savefig("Convergence.png")
 
-print("~~ RUNTIME RESULTS ~~")
-for k,v in timeToRun.items():
-    print(f"\t{k} Neurons took {v[0]} seconds to train and {v[1]} seconds to test")
+plt.scatter(soilData.iloc[:, 0:1].values, soilData.iloc[:, 2:3].values, label='Observed Data, '+'N='+str(n_samples), facecolors='none', edgecolors='r')
+
+plt.semilogy(predicted_DNN5_Y, soilData.iloc[:, 2:3].values, 'k^' ,label=f'{num_neurons}neurons_DNN5: $R^2=$' + str(round(np.sqrt(r2_score(soilData.iloc[:, 0:1].values, predicted_DNN5_Y)),4))+ ', RMSE='+str(round(np.sqrt(mean_squared_error(soilData.iloc[:, 0:1].values, predicted_DNN5_Y)),3))  + '$cm^3$/$cm^3$'+', d1='+str(round(d1_DNN5,4)))
+
+plt.xlabel('Soil Water Content ($cm^3$/$cm^3$)')
+plt.ylabel('Suction (cm)')
+#plt.xlim([0, 0.5])
+# plt.ylim([0, 100])
+from matplotlib.ticker import LogLocator
+
+#plt.legend(loc='lower left')
+#plt.legend(loc='upper right')
+
+ax.legend(ncol=2,handleheight=2.1, labelspacing=0.05, bbox_to_anchor=(0.5, -0.1), loc='upper center')
+
+
+plt.tight_layout()
+# 20. Saave the plots (sic)
+plt.savefig(f'./Output_files/{num_neurons}neurons_NeuralNetwork_Drying1_SWC_5Hidden-Layers_Epoch={training_epochs}.png', bbox_inches="tight")
+## TODO Do we really need PDFs also?
+# plt.savefig(f'./Output_files/{num_neurons}neurons_NeuralNetwork_Drying1_SWC_5Hidden-Layers_Epoch={training_epochs}.pdf', bbox_inches="tight")
+# plt.show()
+sess5.close()
+
+dataDict = {
+    "costs": [float(i) for i in costsSeries],
+    "trainTime": trainTime,
+    "testTime": testTime,
+    "d1": round(d1_DNN5,4),
+    "RSME": round(np.sqrt(mean_squared_error(soilData.iloc[:, 0:1].values, predicted_DNN5_Y)),3),
+    "R2": round(np.sqrt(r2_score(soilData.iloc[:, 0:1].values, predicted_DNN5_Y)),4)
+}
+with open(f"runData/{num_neurons}_neurons.json", "w") as f:
+    json.dump(dataDict, f)
+
+# fig = plt.figure()
+# ax = plt.subplot(111)
+# for (k,v) in costs.items():
+#     plt.plot([i for i in range(0,training_epochs,20)], [i for i in map(lambda x: math.log(x, 10), v)], label=f"{k} Neurons")
+# plt.title(f"Convergence Times")
+# plt.xlabel("Epochs")
+# plt.legend()
+# plt.ylabel("log_10(Cost)")
+# plt.savefig("Convergence.png")
+
+# print("~~ RUNTIME RESULTS ~~")
+# for k,v in timeToRun.items():
+#     print(f"\t{k} Neurons took {v[0]} seconds to train and {v[1]} seconds to test")
---
+++ processData.py
@@ -0,0 +1,69 @@
+import json
+from matplotlib import pyplot as plt
+import math
+
+def readRunData(filepath: str) -> dict:
+    with open(filepath, "r") as f:
+        data = json.load(f)
+    return data
+
+# Process Data
+run3 = readRunData("runData/3_neurons.json")
+run5 = readRunData("runData/5_neurons.json")
+run7 = readRunData("runData/7_neurons.json")
+run9 = readRunData("runData/9_neurons.json")
+run11 = readRunData("runData/11_neurons.json")
+
+# D-1 Results
+fig = plt.figure()
+ax = plt.subplot()
+plt.bar(["3 Neurons","5 Neurons","7 Neurons","9 Neurons","11 Neurons"], [run3["d1"], run5["d1"], run7["d1"], run9["d1"], run11["d1"]], label="d1")
+plt.ylim([.97, 1.01])
+plt.title("d-1 Values For Each Run")
+plt.xlabel("Neurons per Hidden Layer")
+plt.ylabel("d-1")
+plt.savefig("d-1.png")
+
+# RMSE Results
+fig = plt.figure()
+ax = plt.subplot()
+plt.bar(["3 Neurons","5 Neurons","7 Neurons","9 Neurons","11 Neurons"], [run3["RSME"], run5["RSME"], run7["RSME"], run9["RSME"], run11["RSME"]], label="RSME")
+plt.ylim([0, .0031])
+plt.title("RMSE Values For Each Run")
+plt.xlabel("Neurons per Hidden Layer")
+plt.ylabel("RMSE")
+plt.savefig("RMSE.png")
+
+# R2 Results
+fig = plt.figure()
+ax = plt.subplot()
+plt.bar(["3 Neurons","5 Neurons","7 Neurons","9 Neurons","11 Neurons"], [run3["R2"], run5["R2"], run7["R2"], run9["R2"], run11["R2"]], label="R2")
+plt.ylim([.99, .998])
+plt.title("R2 Values For Each Run")
+plt.xlabel("Neurons per Hidden Layer")
+plt.ylabel("R2")
+plt.savefig("R2.png")
+
+# Train time Results
+fig = plt.figure()
+ax = plt.subplot()
+plt.bar(["3 Neurons","5 Neurons","7 Neurons","9 Neurons","11 Neurons"], [run3["trainTime"], run5["trainTime"], run7["trainTime"], run9["trainTime"], run11["trainTime"]], label="trainTime")
+plt.ylim([500, 590])
+plt.title("Training Time For Each Run")
+plt.xlabel("Neurons per Hidden Layer")
+plt.ylabel("Training Time (seconds)")
+plt.savefig("traintime.png")
+
+# Convergence
+fig = plt.figure()
+ax = plt.subplot(111)
+i = 3
+for run in [run3, run5, run7, run9, run11]:
+    v = run["costs"]
+    plt.plot([i for i in range(0,5000,20)], [i for i in map(lambda x: math.log(x, 10), v)], label=f"{i} Neurons")
+    i += 2
+plt.title(f"Convergence Times")
+plt.xlabel("Epochs")
+plt.legend()
+plt.ylabel("log_10(Cost)")
+plt.savefig("Convergence.png")
